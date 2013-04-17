25f183fb516428057e2c5af4fd4ac26649bbde11
Mark Andreas Baagø <Mabaagoe1_4@msn.com>
2013-04-17 13:41:43 +0200

    hej

--- src/semesterprojekt/GUI.java
+++ src/semesterprojekt/GUI.java
@@ -792,9 +792,9 @@ public class GUI extends javax.swing.JFrame {
         String afhentning;
         int kno = 0;
         if (jRadioButtonAfhentning.isSelected()) {
-            afhentning = "Leveres af os";
-        } else {
             afhentning = "Afhentes af kunden";
+        } else {
+            afhentning = "Leveres af os";
         }
 
         if (jComboBox1.getSelectedItem() != "Kunder") {
@@ -1434,6 +1434,7 @@ public class GUI extends javax.swing.JFrame {
                                         jComboBox2.removeItemAt(l);
                                     } else if (o.getTid().equals("Stor Order")) {
                                         jComboBox2.removeAllItems();
+                                        jComboBox2.addItem("Transport");
                                     }
                                 }
                             }
